892e868b6199b06e2649bffcc7493cd9b7b9273f
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-11-15 17:21:09 +0100

    Extends error for Pet type errors

--- src/service/pet.service.ts
+++ src/service/pet.service.ts
@@ -1,5 +1,10 @@
+import { httpErrors } from "@fastify/sensible";
 import { PetToCreate } from "../entity/pet.type";
 import { PetRepository } from "../repository/pet.repository"
+
+export class PetNotFoundError extends Error {}
+
+export class PetTakenError extends Error {}
 
 export class PetService {
   private readonly repository;
@@ -15,7 +20,10 @@ export class PetService {
   async getById(id: number) {
     const pet = await this.repository.readById(id);
     if(!pet) {
-      throw new Error('Pet not found');
+      //should return 404
+      // throw httpErrors.notFound('Pet not found')
+      throw new PetNotFoundError('Pet not found')
+      // throw new Error('Pet not found');
     }
     return pet;
   }
@@ -27,14 +35,23 @@ export class PetService {
   async adopt(petId: number, ownerId: number) {
     const pet = await this.repository.readById(petId);
     if (!pet) {
-      throw new Error('Pet does not exists.');
+      //should return 404
+      // throw httpErrors.notFound('Pet does not exists.')
+      throw new PetNotFoundError('Pet does not exists.')
+      // throw new Error('Pet does not exists.');
     }
     if(pet.ownerId !== null) {
-      throw new Error('Pet has already have an owner.')
+      //should return 400
+      // throw httpErrors.badRequest('Pet has already have an owner.')
+      throw new PetTakenError();
+      // throw new Error('Pet has already have an owner.')
     }
     const adopted = await this.repository.update(petId, { ownerId })
     if (!adopted) {
-      throw new Error('Pet could not be adopted, because it is disappeared.');
+      //should return 400
+      // throw httpErrors.badRequest('Pet could not be adopted, because it is disappeared.');
+      throw new PetNotFoundError('Pet not found')
+      //throw new Error('Pet could not be adopted, because it is disappeared.');
     }
     return adopted;
   }
